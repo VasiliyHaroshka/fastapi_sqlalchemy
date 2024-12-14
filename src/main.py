2e16248254a3377c1dfaac70840f267853c93af6
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-14 21:48:40 +0300

    start project

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8080,
+        reload=True,
+    )
