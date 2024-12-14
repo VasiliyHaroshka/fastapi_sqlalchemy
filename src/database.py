19481fd13b17aefcbea19527cf7ba8d6362bc701
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-14 22:19:30 +0300

    add async_sessionmaker

--- src/database.py
+++ src/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from config import Settings
 
 engine = create_async_engine(
     Settings.db_url,
     echo=True,
+)
+
+session_local = async_sessionmaker(
+    engine,
+    autoflush=True,
+    expire_on_commit=False,
 )
