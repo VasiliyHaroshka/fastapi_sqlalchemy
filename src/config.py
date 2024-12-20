c36d97e3990993f61b9e028dfc886a76e24c60c9
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-20 19:15:33 +0300

    fix bugs

--- src/config.py
+++ src/config.py
@@ -8,12 +8,11 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
 
-    source = SettingsConfigDict(env_file=".env")
-
-    @property
-    def db_url(self):
+    def db_url(self) -> str:
         """Return url to async connect to db"""
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file="/.env")
 
 
 settings = Settings()
--- src/database/database.py
+++ src/database/database.py
@@ -1,11 +1,10 @@
-from sqlalchemy import create_async_engine
-from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from config import Settings
 
 engine = create_async_engine(
-    Settings.db_url,
+    url=Settings.db_url,
     echo=True,
 )
 
