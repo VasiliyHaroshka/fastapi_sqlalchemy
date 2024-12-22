627683b481b2ee5e3a1a241d2345505c7732c2f5
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-22 15:18:49 +0300

    add service get_all_workers

--- src/worker/services.py
+++ src/worker/services.py
@@ -1,16 +1,27 @@
 from fastapi import HTTPException
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from worker.model import Worker
 
 
-async def get_worker(name: str, db: Session):
+async def get_worker(name: str, db: AsyncSession) -> Worker:
     query = select(Worker).filter(Worker.name == name)
     worker = await db.execute(query).scalars().one()
     if not worker:
         raise HTTPException(
             status_code=404,
-            detail=f"Worker with name = {name} is not found"
+            detail=f"Worker with name = {name} is not found",
         )
     return worker
+
+
+async def get_all_workers(db: AsyncSession, limit: int, skip: int) -> list[Worker]:
+    query = select(Worker).offset(skip).limit(limit)
+    workers = await db.execute(query).scalars().all()
+    if not workers:
+        raise HTTPException(
+            status_code=404,
+            detail=f"There is no workers in database",
+        )
+    return workers
