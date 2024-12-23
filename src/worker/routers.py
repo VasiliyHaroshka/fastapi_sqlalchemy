4605d78c644941aec8f4e997dda3b2b63c7246bd
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-23 17:56:06 +0300

    formatted routers and add WorkerCreateSchema

--- src/worker/routers.py
+++ src/worker/routers.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import services
 from database.database import get_db
-from worker.schemas import WorkerGetSchema
+from worker.schemas import WorkerGetSchema, WorkerCreateSchema
 
 router = APIRouter(
     prefix="/worker",
@@ -12,15 +12,25 @@ router = APIRouter(
 
 
 @router.get("/{name}")
-async def get_worker(name: WorkerGetSchema, db: AsyncSession = Depends(get_db)):
+async def get_worker(
+        name: WorkerGetSchema,
+        db: AsyncSession = Depends(get_db),
+    ):
     return services.get_worker(name, db)
 
 
 @router.get("/all")
-async def get_all_workers(db: AsyncSession = Depends(get_db), limit: int = 0, skip: int = 0):
+async def get_all_workers(
+        db: AsyncSession = Depends(get_db),
+        limit: int = 0,
+        skip: int = 0,
+    ):
     return services.get_all_workers(db, limit, skip)
 
 
 @router.post("/create")
-async def create_worker(data, db: AsyncSession = Depends(get_db))
+async def create_worker(
+        data: WorkerCreateSchema,
+        db: AsyncSession = Depends(get_db),
+    ):
     return services.create_worker(data, db)
