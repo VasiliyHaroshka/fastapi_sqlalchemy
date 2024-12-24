9e752c00cc74980e19abf5b264c1dd6d7c71e591
VasiliyHaroshka <horvasiliy@mail.ru>
2024-12-24 16:20:41 +0300

    replace Asyncsession to Sessionlocal

--- src/worker/routers.py
+++ src/worker/routers.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm.sync import update
 
 import services
-from database.database import get_db
+from database.database import get_db, SessionLocal
 from worker.schemas import WorkerGetSchema, WorkerCreateSchema
 
 router = APIRouter(
@@ -14,23 +14,23 @@ router = APIRouter(
 @router.get("/{name}")
 async def get_worker(
         name: WorkerGetSchema,
-        db: AsyncSession = Depends(get_db),
-    ):
+        db: SessionLocal = Depends(get_db),
+):
     return services.get_worker(name, db)
 
 
 @router.get("/all")
 async def get_all_workers(
-        db: AsyncSession = Depends(get_db),
+        db: SessionLocal = Depends(get_db),
         limit: int = 0,
         skip: int = 0,
-    ):
+):
     return services.get_all_workers(db, limit, skip)
 
 
 @router.post("/create")
 async def create_worker(
         data: WorkerCreateSchema,
-        db: AsyncSession = Depends(get_db),
-    ):
+        db: SessionLocal = Depends(get_db),
+):
     return services.create_worker(data, db)
