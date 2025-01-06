80a8aefa5fb54aff0349f2c62d932d41bb685bb3
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-06 11:24:17 +0300

    add router get_resume_by_title

--- src/resume/routers.py
+++ src/resume/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.util import await_only
 
 from database.database import get_db, SessionLocal
 from error import Missing
+from resume import services
 from resume.model import Resume
 
 router = APIRouter(
@@ -15,8 +17,16 @@ async def get_all_resumes(
         db: SessionLocal = Depends(get_db),
         limit: int = 0,
         skip: int = 0,
-    ):
+):
     try:
-        return await service.get_all_resumes(db, limit, skip)
+        return await services.get_all_resumes(db, limit, skip)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
+
+
+@router.get("/{title}")
+async def get_resume_by_title(title: str) -> Resume:
+    try:
+        return await services.get_resume_by_title(title)
     except Missing as e:
         raise HTTPException(status_code=404, detail=e.msg)
