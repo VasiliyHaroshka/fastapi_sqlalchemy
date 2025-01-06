f5bdd9f3215ca598883c7711ecc33ab7b4b5a6c6
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-06 11:07:02 +0300

    create and connect resume router

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from worker.routers import router as worker_router
+from resume.routers import router as resume_router
 
 app = FastAPI()
 
 app.include_router(worker_router)
+app.include_router(resume_router)
 
 if __name__ == "__main__":
     import uvicorn
