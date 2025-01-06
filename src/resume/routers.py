e2da02def25d99cc3687c0023d457bbbdba203a6
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-06 11:14:08 +0300

    add get all resumes router

--- src/resume/routers.py
+++ src/resume/routers.py
@@ -1,6 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+
+from database.database import get_db, SessionLocal
+from error import Missing
+from resume.model import Resume
 
 router = APIRouter(
-    prefix="/resime",
+    prefix="/resume",
     tags=["Resume"],
 )
+
+
+@router.get("/all")
+async def get_all_resumes(
+        db: SessionLocal = Depends(get_db),
+        limit: int = 0,
+        skip: int = 0,
+    ):
+    try:
+        return await service.get_all_resumes(db, limit, skip)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
