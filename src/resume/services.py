63a2b5f820dca9c5136fe1e73b9ba554d6beb05f
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-06 11:30:29 +0300

    add get_resumes_by_title service

--- src/resume/services.py
+++ src/resume/services.py
@@ -13,3 +13,13 @@ async def get_all_resumes(db: AsyncSession, limit: int, skip: int) -> list[Resum
             msg=f"There is no resume in database",
         )
     return [resume for resume in result.scalars().all()]
+
+
+async def get_resumes_by_title(title: str, db: AsyncSession) -> list[Resume]:
+    query = select(Resume).filter_by(title=title)
+    result = await db.execute(query)
+    if not result:
+        raise Missing(
+            msg=f"There is no resume with title = {title} in database",
+        )
+    return [resume for resume in result.scalars().all()]
