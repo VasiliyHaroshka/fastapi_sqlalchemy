c65b777a8a4aab7f2f082e1ad546f52cbd4d23f1
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-08 17:42:03 +0300

    add CreateResumeSchema

--- src/resume/schemas.py
+++ src/resume/schemas.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
+from resume.model import Workload
 
-class GetResumesByName(BaseModel):
+
+class GetResumesByNameSchema(BaseModel):
     title: str
+
+
+class CreateResumeSchema(GetResumesByNameSchema):
+    description: str
+    salary: int | None
+    workload: Workload
