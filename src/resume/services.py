01efebada8d010cee92380f9319b1a0328e6ee41
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-08 18:01:25 +0300

    add service create resume

--- src/resume/services.py
+++ src/resume/services.py
@@ -1,9 +1,10 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.util import await_only
 
-from error import Missing
+from error import Missing, Duplicate
 from resume.model import Resume
-from resume.schemas import GetResumesByName
+from resume.schemas import GetResumesByNameSchema, CreateResumeSchema
 
 
 async def get_all_resumes(db: AsyncSession, limit: int, skip: int) -> list[Resume]:
@@ -17,12 +18,11 @@ async def get_all_resumes(db: AsyncSession, limit: int, skip: int) -> list[Resum
 
 
 async def get_resumes_by_title(
-        title: GetResumesByName,
+        title: GetResumesByNameSchema,
         db: AsyncSession,
         limit,
         skip,
 ) -> list[Resume]:
-
     query = select(Resume).filter_by(title=title).offset(skip).limit(limit)
     result = await db.execute(query)
     if not result:
@@ -30,3 +30,20 @@ async def get_resumes_by_title(
             msg=f"There is no resume with title = {title} in database",
         )
     return [resume for resume in result.scalars().all()]
+
+
+async def create_resume(data: CreateResumeSchema, db: AsyncSession) -> Resume:
+    new_resume = Resume(
+        title=data.title,
+        description=data.description,
+        salary=data.salary,
+        workload=data.workload,
+    )
+    all_resumes = select(Resume)
+    if new_resume in all_resumes:
+        raise Duplicate(msg="This resume already exists in database")
+    db.add(new_resume)
+    await db.commit()
+    query = select(Resume).filter_by(id=new_resume.id)
+    result = await db.execute(query)
+    return result.scalars().one()
