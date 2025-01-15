1e5ef6b67ac4c28a6f61efad1625f6838d7e14f0
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-15 19:05:38 +0300

    add relationship to projects model

--- src/projects/model.py
+++ src/projects/model.py
@@ -1,13 +1,19 @@
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.database import Base
+
 
 class Project(Base):
     __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(250))
+
+    worker_in: Mapped[list["Workers"]] = relationship(
+        back_populates="workers_projects",
+        secondary="workers_projects",
+    )
 
 
 class WorkersProjects(Base):
