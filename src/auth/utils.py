30386505343185d671fec87bb89cee1628cfbd5d
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-30 20:03:20 +0300

    add type annotations and default values to encode and decode in auth

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -4,28 +4,28 @@ from config import settings
 
 
 def encode_jwt_token(
-        payload,
-        key,
-        algorithm,
+        payload: dict,
+        private_key: str = settings.jwt_auth.private_key_path.read_text(),
+        algorithm: str = settings.jwt_auth.algorithm,
 ):
     """Return encoded jwt token"""
     encoded = jwt.encode(
         payload=payload,
-        key=key,
+        key=private_key,
         algorithm=algorithm,
     )
     return encoded
 
+
 def decode_jwt_token(
-    token,
-    puclic_key,
-    algorithm,
+        token: str | bytes,
+        public_key: str = settings.jwt_auth.public_key_path,
+        algorithm: str = settings.jwt_auth.algorithm,
 ):
     """Return decoded jwt token"""
     decoded = jwt.decode(
         token,
-        puclic_key,
+        public_key,
         algorithms=[algorithm],
     )
     return decoded
-
