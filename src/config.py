f67af5eb44c7ad581832f2fd394905c586006309
VasiliyHaroshka <horvasiliy@mail.ru>
2025-01-30 19:49:23 +0300

    add JWTAuth to Settings in config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,16 @@
+from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class JWTAuth(BaseModel):
+    private_key_path: Path = BASE_DIR / "certificates" / "private.pem"
+    public_key_path: Path = BASE_DIR / "certificates" / "public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
 
 
 class Settings(BaseSettings):
@@ -12,6 +24,8 @@ class Settings(BaseSettings):
     def db_url(self) -> str:
         """Return url to async connect to db"""
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    jwt_auth: JWTAuth = JWTAuth()
 
     model_config = SettingsConfigDict(env_file="../.env")
 
