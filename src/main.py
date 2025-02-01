f7d179da655598221a23c9d1491b4a50fc713567
VasiliyHaroshka <horvasiliy@mail.ru>
2025-02-01 11:01:34 +0300

    connect auth router to main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from auth.routers import router as auth_router
 from database.database import Base, engine
 from worker.routers import router as worker_router
 from resume.routers import router as resume_router
@@ -10,6 +11,7 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(worker_router)
 app.include_router(resume_router)
+app.include_router(auth_router)
 
 if __name__ == "__main__":
     import uvicorn
