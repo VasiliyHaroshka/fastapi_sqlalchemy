dfba1d04f3b4db23fde2d59a65de565376924a0d
VasiliyHaroshka <horvasiliy@mail.ru>
2025-02-01 11:57:47 +0300

    add unactive_exception

--- src/auth/error.py
+++ src/auth/error.py
@@ -3,7 +3,15 @@ from fastapi import HTTPException, status
 
 def unauth_exception():
     unauth_error = HTTPException(
-        status_code=401,
+        status_code=status.HTTP_401_UNAUTHORIZED,
         detail="wrong username or password"
     )
-    raise unauth_error
+    return unauth_error
+
+
+def unactive_exception():
+    unactive_error = HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="user is not active"
+    )
+    return unactive_error
