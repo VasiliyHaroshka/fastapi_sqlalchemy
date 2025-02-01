0839a9c7b14a2194893d2747b3200b39446643f8
VasiliyHaroshka <horvasiliy@mail.ru>
2025-02-01 11:59:50 +0300

    add user_validator to login

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 
 from auth import utils
 from auth.schema import TokenSchema
+from auth.validators import user_validator
 from user.schemas import UserSchema
 
 router = APIRouter(
@@ -11,7 +12,7 @@ router = APIRouter(
 
 
 @router.post("/login/", response_model=TokenSchema)
-def login(user: UserSchema):
+def login(user: UserSchema = Depends(user_validator)):
     payload = {
         "sub": user.id,
         "username": user.username,
