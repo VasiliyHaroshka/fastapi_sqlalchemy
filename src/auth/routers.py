b193da0b4dd9cfc95fc4f8e6995360df87633108
VasiliyHaroshka <horvasiliy@mail.ru>
2025-02-03 18:41:42 +0300

    add router self_info

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 
 from auth import utils
 from auth.schema import TokenSchema
+from auth.utils import get_current_auth_user, get_payload_from_credentials
 from auth.validators import user_validator
 from user.schemas import UserSchema
 
@@ -23,3 +24,16 @@ def login(user: UserSchema = Depends(user_validator)):
         access_token=access_token,
         token_type="Bearer",
     )
+
+
+@router.get("/me")
+def self_info(
+        user: UserSchema = Depends(get_current_auth_user),
+        payload: dict = Depends(get_payload_from_credentials),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "login_at": iat,
+    }
